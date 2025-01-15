85447188cfa2f1513dea32abc94e07c5e8ac4d4c
nozaki59 <107387074+nozaki59@users.noreply.github.com>
2025-01-15 16:49:20 +0900

    add: csvインポート時の上書き設定

--- app/Imports/ShiftsImport.php
+++ app/Imports/ShiftsImport.php
@@ -10,12 +10,32 @@ class ShiftsImport implements ToModel
 {
     public function model(array $row)
     {
+
+        // 勤務時間のnullをチェック
+        $start_time = !empty($row[10]) ? \Carbon\Carbon::parse($row[10])->format('H:i') : null;
+        $end_time = !empty($row[12]) ? \Carbon\Carbon::parse($row[12])->format('H:i') : null;
+
+        // 該当する社員IDと日付のデータを検索
+        $shift = Shift::where('employee_id', $row[1])
+            ->where('shift_date', \Carbon\Carbon::parse($row[2])->format('Y-m-d'))
+            ->first();
+
+        if ($shift) {
+            // 既存データがある場合は更新
+            $shift->update([
+                'shift_type' => $row[7],
+                'start_time' => $start_time,
+                'end_time' => $end_time,
+            ]);
+            return null; // 更新時は新しいモデルを返さない
+        }
+
         return new Shift([
             'employee_id' => $row[1],  // CSVの社員ID列名を確認して置き換えてください
             'shift_date' => \Carbon\Carbon::parse($row[2])->format('Y-m-d'),  // 日付の形式を適切に変換
             'shift_type' => $row[7],
-            'start_time' => \Carbon\Carbon::parse($row[10])->format('H:i'),  // 時刻形式を適切に変換
-            'end_time' => \Carbon\Carbon::parse($row[12])->format('H:i'),  // 時刻形式を適切に変換
+            'start_time' => $start_time,  // 時刻形式を適切に変換
+            'end_time' => $end_time,  // 時刻形式を適切に変換
         ]);
     }
 }
--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -1,9 +1,11 @@
-<x-template title="PMシフト表" css=".css">
-    <div>
-        <form action="{{ route('shifts.import') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <input type="file" name="file" required>
-            <button type="submit">インポート</button>
-        </form>
+<x-template title="PMシフト表" css="import.css">
+    <div class="flex justify-center items-center w-full min-h-[100dvh]">
+        <div class="md:w-2/3 w-full h-[300px] py-5 flex justify-center items-center bg-orange-100">
+            <form action="{{ route('shifts.import') }}" method="POST" enctype="multipart/form-data" class="max-md:flex max-md:flex-col max-md:justify-center max-md:items-center">
+                @csrf
+                <input type="file" name="file" required>
+                <button type="submit" class="btn shadow px-3 py-2 bg-gray-300 max-md:mt-10">インポート</button>
+            </form>
+        </div>
     </div>
 </x-template>
